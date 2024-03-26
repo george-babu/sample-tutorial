d1cbf720f336e881612404a12a9c5b73508035f7
George Babu <george.babu@gov.bc.ca>
2024-03-26 12:37:30 -0700

    env added

--- sample_tutorial/settings.py
+++ sample_tutorial/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tvnm8ck_l+)^ui$1r8$upnbs_ayzs8q^m1j!-%@8v958yz#*v9'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,10 +78,10 @@ WSGI_APPLICATION = 'sample_tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sampletutorialdb', 
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '10.97.187.127', 
+        'NAME': os.getenv("DB_NAME"), 
+        'USER': os.getenv("DB_USERNAME"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"), 
         'PORT': '5432',
     }
 }
